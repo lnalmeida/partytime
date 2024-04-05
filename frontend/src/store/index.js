e5adce8c8ad22aecc48c533c6d1472640966d798
LUIZ NUNES DE ALMEIDA JUNIOR <l.n.almeida.ti@gmail.com>
2024-04-05 12:46:18 -0300

    vuex store config

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,36 @@
 import { createStore } from 'vuex'
+import VuexPersistence from 'vuex-persist'
+
+const vexLocal = new VuexPersistence({
+  storage: window.localStorage
+});
 
 export default createStore({
-  state: {
+  state() {
+    return {
+      isAutenthicated: false,
+      token: null,
+      userId: null
+    }
   },
   getters: {
+    isAutenthicated:  state => state.isAutenthicated,
+    token: state => state.token,
+    userId: state => state.userId
   },
   mutations: {
+    authenticate(state, data) {
+      state.isAutenthicated  = true;
+      state.token = data.token,
+      state.userId = data.userId
+    },
+    logout(state) {
+      state.isAutenthicated =  false,
+      state.token =  null,
+      state.userId =  null
+    }
   },
   actions: {
   },
-  modules: {
-  }
+  modules: [vuexLocal.plugin]
 })
